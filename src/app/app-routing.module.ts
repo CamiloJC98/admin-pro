4d7651bfa72c1df1c043ef19e385d96a5c6afb4e
Juan Camilo Martinez Henao <camilo.martinez12@outlook.com>
2022-02-09 21:42:42 -0500

    Feat: Se agregan las rutas y modulos independiente

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+const routes: Routes = [
+
+  // path: 'dashboard' PagesRouting
+  // path: 'auth' AuthRouting
+
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent },
+
+];
+
+@NgModule({
+  imports: [
+    AuthRoutingModule,
+    PagesRoutingModule,
+    RouterModule.forRoot(routes),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
